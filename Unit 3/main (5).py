3d0a8cc343035af501c5925c61bb02c4b367aaff
asper105per10522cs29 <144481233+asper105per10522cs29@users.noreply.github.com>
2023-10-28 23:38:56 -0700

    Add files via upload

---
+++ Unit 3/main (5).py
@@ -0,0 +1,17 @@
+def fact_rec(n):
+
+if n==0 or n==1:
+
+return 1
+
+else:
+
+return n*fact_rec(n-1)
+
+number =int(input("Enter a Value:"))
+
+0.00
+
+LTE
+
+res =fact_rec(number) print("The factorial of } is {},"format (number,res))
